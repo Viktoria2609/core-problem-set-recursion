6376fd0fc11e8bcd2d52b15079bc210ee1050de8
Viktoriia <lyudkevych.v@gmail.com>
2025-05-05 01:02:43 -0700

    Part-1 and Part-2 completed

--- part-2.py
+++ part-2.py
@@ -4,13 +4,33 @@
 # the appropriate comment.
 
 # search
-
-
+def search(array, query):
+    if len(array) == 0:
+        return False
+    if array[0] == query:
+        return True
+    return search(array[1:], query)
 
 # is_palindrome
-
+def is_palindrome(text):
+    if len(text) <= 1:
+        return True
+    if text[0] == text[-1]:
+        return is_palindrome(text[1:-1])
+    return False
 
 
 # digit_match
+# I didn't pass test. Still working on it
+def digit_match(num1, num2):
+    if num1 == 0 and num2 == 0:
+        return 1
 
+    if num1 == 0 or num2 == 0:
+        return 0
+
+    if num1 % 10 == num2 % 10:
+        return 1 + digit_match(num1 // 10, num2 // 10)
+    else:
+        return digit_match(num1 // 10, num2 // 10)
 
