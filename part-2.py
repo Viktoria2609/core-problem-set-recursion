97b827863b0bb9f392b4d0c87659d50a1e4467a2
Viktoriia <lyudkevych.v@gmail.com>
2025-05-08 22:53:07 -0700

    Digit_match function updateted.All tests passed

--- part-2.py
+++ part-2.py
@@ -21,16 +21,15 @@ def is_palindrome(text):
 
 
 # digit_match
-# I didn't pass test. Still working on it
 def digit_match(num1, num2):
     if num1 == 0 and num2 == 0:
         return 1
 
-    if num1 == 0 or num2 == 0:
-        return 0
+    def inner(n1, n2):
+        if n1 == 0 or n2 == 0:
+            return 0
+        match = 1 if n1 % 10 == n2 % 10 else 0
+        return match + inner(n1 // 10, n2 // 10)
 
-    if num1 % 10 == num2 % 10:
-        return 1 + digit_match(num1 // 10, num2 // 10)
-    else:
-        return digit_match(num1 // 10, num2 // 10)
+    return inner(num1, num2)
 
